417691e02f58ddbde7d442625695857b43c7cacb
Maciej Tomaszewski <mt.maciejtomaszewski@gmail.com>
2023-12-04 23:59:04 +0100

    feat(homepage): done home page mobile design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        BubbleBobble: ['BubbleBobble', 'sans-serif'],
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
@@ -65,10 +68,21 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
+        'levitate': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-5px)' },
+        },
+        'levitate-slow': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-5px)' },
+        },
+
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'levitate': 'levitate 2s ease-in-out infinite',
+        'levitate-slow': 'levitate 4s ease-in-out infinite',
       },
     },
   },
