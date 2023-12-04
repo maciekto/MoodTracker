93b39a2b7bd22cdb2ad48fdb3d101292e9bd9424
Kamil Szczerbiak <kamil.szczerbiak@shop-apotheke.com>
2023-12-04 20:28:51 +0100

    feat: add shadcn, fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,16 +23,6 @@ const config = {
       'LabeledStatement',
       'WithStatement',
     ],
-    'no-unused-vars': [
-      1,
-      {
-        ignoreRestSiblings: true,
-        argsIgnorePattern: 'res|next|^err|^_',
-        varsIgnorePattern: '^_',
-        // Broken in TypeSCript.Want this turned on
-        // destructuredArrayIgnorePattern: '^_',
-      },
-    ],
     'prefer-const': [
       'error',
       {
@@ -64,7 +54,6 @@ const config = {
     'import/extensions': 0,
     'no-underscore-dangle': 0,
     'consistent-return': 0,
-    'react/display-name': 1,
     'react/no-array-index-key': 0,
     'react/react-in-jsx-scope': 0,
     'react/prefer-stateless-function': 0,
@@ -72,19 +61,7 @@ const config = {
     'react/no-unescaped-entities': 0,
     'react/function-component-definition': 0,
     'jsx-a11y/accessible-emoji': 0,
-    'jsx-a11y/label-has-associated-control': [
-      'error',
-      {
-        assert: 'either',
-      },
-    ],
     'react/require-default-props': 0,
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.mdx'],
-      },
-    ],
     radix: 0,
     'no-shadow': [
       2,
@@ -101,23 +78,7 @@ const config = {
         allowTemplateLiterals: true,
       },
     ],
-    'prettier/prettier': [
-      'error',
-      {
-        'singleQuote': true,
-        'endOfLine': 'auto',
-        'tabWidth': 4
-      },
-    ],
     'jsx-a11y/href-no-hash': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'warn',
-      {
-        aspects: ['invalidHref'],
-      },
-    ],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
     '@typescript-eslint/comma-dangle': ['off'],
     'react/jsx-props-no-spreading': 'off',
   },
