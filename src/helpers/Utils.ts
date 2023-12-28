e04725561c2f57ab73b7b3bd9e12cad281a8f36b
Kamil Szczerbiak <kamil.szczerbiak@shop-apotheke.com>
2023-12-28 16:19:11 +0100

    fix: fix warnings

--- src/helpers/Utils.ts
+++ src/helpers/Utils.ts
@@ -13,25 +13,26 @@ const MONTHS = [
   'December',
 ];
   
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const months = (config: any) => {
   const cfg = config || {};
   const count = cfg.count || 12;
   const section = cfg.section;
   const start = cfg.start || 0;
-  const values = [];
+  const values: string[] = [];
   // eslint-disable-next-line no-shadow
   const months = cfg.months || null
   let i, value;
   if(months) {
     months.forEach((month: number) => {
       value = MONTHS[Math.ceil(month) % 12];
-      values.push(value.substring(0, section));
+      values.push((value ?? '').substring(0, section));
     })
     return values;
   }
   for (i = start; i < count; ++i) {
     value = MONTHS[Math.ceil(i) % 12];
-    values.push(value.substring(0, section));
+    values.push((value ?? '').substring(0, section));
   }
   
   return values;
